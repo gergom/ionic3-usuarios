75c9036394d40de1178dffe994e59d8bc03243fe
Augusto Gomez <gagb_86@hotmail.com>
2018-09-29 00:01:32 -0500

    Ionic 3 - Usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {HttpClientModule} from "@angular/common/http";
+import { UserServiceProvider } from '../providers/user-service/user-service';
 
 @NgModule({
   declarations: [
@@ -25,7 +27,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    UserServiceProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { UserServiceProvider } from '../../providers/user-service/user-service';
+
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public userService: UserServiceProvider) {
 
-  }
+    }
 
+    users = null;
+
+    ionViewDidLoad(){
+        this.userService.getUsers()
+            .subscribe(
+                (data) => { // Success
+                    this.users = data['results'];
+                },
+                (error) =>{
+                    console.error(error);
+                }
+            )
+    }
 }
